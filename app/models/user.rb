ca38d9af452ad419c66b49e0f1e3167327bec518
Katayose Koki <1911140032m@ed.fuk.kindai.ac.jp>
2021-10-24 15:15:49 +0000

    L5

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  #TODO: validates
+  has_many :tweets, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :like_tweets, source: :tweet, through: :likes
+  
+  validates :password, presence: true, confirmation: true
+  
+  attr_accessor :password, :password_confirmation
+  
+  def password=(val)
+   if val.present?
+    self.pass = BCrypt::Password.create(val)
+   end
+   @password = val
+  end
+end
